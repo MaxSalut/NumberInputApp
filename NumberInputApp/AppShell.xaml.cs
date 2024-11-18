c58a4d72610cb1aceb1feb1662d560f056ae6292
MaxSalut <m.bezverkhyi28@gmail.com>
2024-11-18 02:02:52 +0200

    Add project files.

---
+++ NumberInputApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NumberInputApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NumberInputApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace NumberInputApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnShowNumberClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberEntry.Text, out int number))
+            {
+                DisplayAlert("Результат", $"Ви ввели число {number}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Помилка", "Будь ласка, введіть коректне ціле число.", "OK");
+            }
+        }
+    }
+}
